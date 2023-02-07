83c8770d5324cf1a00ec034d1c5855d0348ebfb7
daveS <davesimpcontrol@gmail.com>
2023-02-06 21:06:07 -0700

    placeholders for getting elements applied

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,28 @@
-//psuedo code
+const apiUrl = 'https://api.openbrewerydb.org/breweries?by_city=';
 
-// input city of choice
-    // capture input of city 
+async function getBreweries(city) {
+  const response = await fetch(apiUrl + city + '&per_page=3');
+  const data = await response.json();
+  return data;
+}
 
-// pull current weather & 5 day forecast
-// pull list of local brewery's 
+document.getElementById('output').innerHTML = lengthOfName;
+
+async function displayBreweries() {
+  const city = prompt("Enter the city:");
+  const breweries = await getBreweries(city);
+  for (const brewery of breweries) {
+    console.log(`Name: ${brewery.name}`);
+    console.log(`Address: ${brewery.street}, ${brewery.city}, ${brewery.state}`);
+    console.log(`Phone: ${brewery.phone}`);
+    console.log(`Website: ${brewery.website_url}`);
+    console.log("---");
+
+    // document.getElementById('Name').innerHTML = lengthOfName;
+    // document.getElementById('Address').innerHTML = lengthOfAdress;
+    // document.getElementById('Phone').innerHTML = lengthOfPhone;
+    // document.getElementById('Website').innerHTML = lengthOfWebsite;
+  }
+}
+
+displayBreweries();
