2ab9a49f06f3aa1f0803a9de6be9af2221aed119
daveS <davesimpcontrol@gmail.com>
2023-02-13 21:10:08 -0700

    polished

--- assets/aPotentBrew.js
+++ assets/aPotentBrew.js
@@ -4,28 +4,32 @@ window.onload = function() {
   const searchInput = document.querySelector("input");
 
 
+// file names array // for each// 
 
 function populateCard(brewery) {
   const breweryResults = document.getElementById ("brewery-results")
   
+
   const cardCol = document.createElement ("div")
   const card = document.createElement("div");
 
-  const cardName = document.createElement("h5")
+  const cardName = document.createElement("h4")
   const cardImg = document.createElement("img")
   const cardAddress = document.createElement("p")
   const cardPhone = document.createElement("p")
   const cardWebsite = document.createElement("a")
   
-  cardImg.src = "./assets/images/monica-di-loxley-pJLmctCUmW0-unsplash.jpg"
 
 
-  const nameText = document.createTextNode(brewery.name)
-  const addressText = document.createTextNode(brewery.street + "," + brewery.state)
+  cardImg.src = "./assets/images/brew1.jpg"
+
+
+  const nameText = document.createTextNode(brewery.name ?? "")
+  const addressText = document.createTextNode(  brewery.street ? brewery.street + "," + brewery.state : brewery.state ?? "")
   // make the address copy instantly from clicking on it
-  const phoneText = document.createTextNode(brewery.phone)
+  const phoneText = document.createTextNode(brewery.phone ?? "")
   //make the phone # show up with dashes
-  const websiteText = document.createTextNode(brewery.website_url)
+  const websiteText = document.createTextNode(brewery.website_url ?? "")
 
   console.log(brewery);
 
@@ -49,26 +53,26 @@ cardPhone.append(addressText)
 cardAddress.append(phoneText)
 cardWebsite.append(websiteText)
 }
-
-  searchBtn.addEventListener("click", function() {
+searchBtn.addEventListener("click", function() {
     localStorage.setItem("city", searchInput.value);
     const city = localStorage.getItem("city");
     console.log("fetch");
     
     // let text = document.getElementById("text").innerText;
-  
+    document.getElementById("brewery-results").innerHTML = "";
+    
     fetch(apiUrl + city)
-      .then(response => response.json())
-      .then(data => {
-        const breweries = data.slice(0, 3)
-        breweries.forEach((brewery, i) => {
-          console.log(brewery);
-          populateCard(brewery)
-          //clear previous
-        });
-      })
+    
+    .then(response => response.json())
+    .then(data => {
+      const breweries = data.slice(0, 3)
+      breweries.forEach((brewery, i) => {
+        console.log(brewery);
+        populateCard(brewery)
+      });
+    })
       .catch(error => console.error(error));
     });
-    //return;
+
   };
   
