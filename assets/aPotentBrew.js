ee8479e7ae045ac52b8f629c72f0b94c729b1bcd
daveS <davesimpcontrol@gmail.com>
2023-02-13 23:00:48 -0700

    pictures work

--- assets/aPotentBrew.js
+++ assets/aPotentBrew.js
@@ -1,49 +1,72 @@
+
+
 window.onload = function() {
   const apiUrl = 'https://api.openbrewerydb.org/breweries?by_city=';
   const searchBtn = document.querySelector("button");
   const searchInput = document.querySelector("input");
 
-function populateCard(brewery) {
+  function populateCard(brewery) {
 
-  const breweryResults = document.getElementById ("brewery-results")
-
-  const cardCol = document.createElement ("div")
-  const card = document.createElement("div");
-
-  const cardName = document.createElement("h4")
-  const cardImg = document.createElement("img")
-  const cardAddress = document.createElement("p")
-  const cardPhone = document.createElement("p")
-  const cardWebsite = document.createElement("a")
-
-  // cardImg.src = "./assets/images/brew1.jpg"
-
-  const nameText = document.createTextNode(brewery.name ?? "")
-  const addressText = document.createTextNode(  brewery.street ? brewery.street + "," + brewery.state : brewery.state ?? "")
-  const phoneText = document.createTextNode(brewery.phone ?? "")
-  const websiteText = document.createTextNode(brewery.website_url ?? "")
-
-cardCol.className = "col"
-card.className = "brewcards card"
-cardName.className = "card-title"
-cardImg.className = "card-img-top"
-
-card.append(cardName)
-card.append(cardImg)
-card.append(cardAddress)
-card.append(cardPhone)
-card.append(cardWebsite)
-
-cardCol.append(card)
-breweryResults.append(cardCol)
-
-cardName.append(nameText)
-cardPhone.append(addressText)
-cardAddress.append(phoneText)
-cardWebsite.append(websiteText)
-}
-searchBtn.addEventListener("click", function() {
+    const imageFileNames = [
+      "brew1.jpg",
+      "brew2.jpg",
+      "brew3.jpg",
+      "brew4.jpg",
+      "brew5.jpg"
+    ];
+    
+    const breweryResults = document.getElementById ("brewery-results")
   
+    const cardCol = document.createElement("div")
+    const card = document.createElement("div");
+  
+    const cardName = document.createElement("h5")
+    const cardImg = document.createElement("img")
+    const cardAddress = document.createElement("p")
+    const cardPhone = document.createElement("p")
+    const cardWebsite = document.createElement("a")
+  
+    if (typeof populateCard.index === 'undefined') {
+      populateCard.index = 0;
+    } else {
+      populateCard.index = (populateCard.index + 1) % imageFileNames.length;
+    }
+    const index = populateCard.index;
+    cardImg.src = "./assets/images/" + imageFileNames[index];
+  
+    const nameText = document.createTextNode(brewery.name ?? "")
+    const addressText = document.createTextNode(brewery.street ? brewery.street + "," + brewery.state : brewery.state ?? "")
+    // make the address copy instantly from clicking on it
+    const phoneText = document.createTextNode(brewery.phone ?? "")
+    //make the phone # show up with dashes
+    const websiteText = document.createTextNode(brewery.website_url ?? "")
+  
+    console.log(brewery);
+  
+    cardCol.className = "col"
+    card.className = "brewcards card"
+    cardName.className = "card-title"
+    cardImg.className = "card-img-top"
+  
+    card.append(cardName)
+    card.append(cardImg)
+    card.append(cardAddress)
+    card.append(cardPhone)
+    card.append(cardWebsite)
+  
+    cardCol.append(card)
+    breweryResults.append(cardCol)
+  
+    cardName.append(nameText)
+    // cardImg.append(img)
+    cardPhone.append(addressText)
+    cardAddress.append(phoneText)
+    cardWebsite.append(websiteText)
+  }
+  
+  
+searchBtn.addEventListener("click", function() {
+
     localStorage.setItem("city", searchInput.value);
     const city = localStorage.getItem("city");
     document.getElementById("brewery-results").innerHTML = "";
